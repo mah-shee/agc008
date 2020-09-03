d20ff139a6fd8ada42644f0e0912627ae9f81d22
masa <masatoshi.sboard@gmail.com>
2020-09-04 08:11:38 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,19 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        x: isize,
+        y: isize,
     }
+    let mut ans = (y.abs() - x.abs()).abs();
+    if x + ans == y {
+        ans += 0;
+    } else if -x + ans == y {
+        ans += 1;
+    } else if -x - ans == y {
+        ans += 1;
+    } else if x - ans == y {
+        ans += 2;
+    }
+
+    println!("{}", ans);
 }
